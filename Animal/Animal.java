467eab6a8f240c0b15e86684d38ca1b56f8b0058
Anne Emering <anne.emering@gmail.com>
2021-06-16 17:48:06 +0200

    Rearrange files. Define attributes of Animal.

---
+++ Animal/Animal.java
@@ -0,0 +1,23 @@
+public class Animal {
+  // attributes
+  private String species, name, size, age;
+
+  public String getSpecies(){
+    return species;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setSpecies(String species){
+    this.species = species;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+
+
+  }
